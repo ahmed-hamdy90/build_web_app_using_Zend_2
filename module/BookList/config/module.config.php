e8a568b1e57f115b6e5dcd56d998d30bd6d3358b
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2015-07-12 23:23:53 +0200

    Controller and Views part: Create BookController class with index,add,edit and delete Actions, create views for index,add,edit and delete Actions into BookController And modified in config/module.config.php file for Create book route configuation

--- module/BookList/config/module.config.php
+++ module/BookList/config/module.config.php
@@ -1,5 +1,32 @@
 <?php 
 
 return array(
-    
+    'controllers' => array(
+          'invokables' => array(
+               'BookList\Controller\Book' => 'BookList\Controller\BookController',
+          ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'book' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route' => '/book[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'BookList\Controller\Book',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'book' => __DIR__ . '/../view',
+        ),
+    ),
 );
---
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BookList\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class BookController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function addAction()
+    {
+
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+}
