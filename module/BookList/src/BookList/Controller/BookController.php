f5e2092f61124cb672fa241c9a6f4eb24c4e7e5b
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2015-07-14 00:56:29 +0200

    Controller and View part: Update Delete view, Update Delete view,  Write Edit logic (only in Get verb) into Edit Action And Write Delete logic (only in Get verb) into Delete Action in Book Controller

--- module/BookList/src/BookList/Controller/BookController.php
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -24,13 +24,16 @@ class BookController extends AbstractActionController
      * Add Action
      * <br/> Responsible for :
      *       <br/>In case Get verb  -> Display Add new Book Form
-     *       <br/>In case Post verb -> Add new book details into Database after Submit Book Form
+     *       <br/>In case Post verb -> Add new book details into Database
+     *                                 After Submit Book Form
      * @return ViewModel add view
      */
     public function addAction()
     {
         $form = new BookForm();
             $form->get('submit')->setValue('Add');
+            // Another way to set Submit button value
+            // $form->get('submit')->setAttribute('value', 'Add');
 
         $request = $this->getRequest();
         // Check If request is Post verb
@@ -43,14 +46,61 @@ class BookController extends AbstractActionController
         ));
     }
 
+    /**
+     * Edit Action
+     * <br/> Responsible for :
+     *       <br/>In case Get verb  -> Display Edit Exists Book Form
+     *       <br/>In case Post verb -> Edit Exists book with new submitted details
+     *                                 and updated book details into Database
+     *                                 After Submit Book Form
+     * @return ViewModel edit view
+     */
     public function editAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $form = new BookForm();
+            //$form->bind($book);
+            $form->get('submit')->setValue('Edit');
 
+        $request = $this->getRequest();
+        // Check If Request Is Post Verb
+        if ($request->isPost()) {
+
+        }
+
+        return new ViewModel(array(
+            'id'   => $id,
+            'form' => $form
+        ));
     }
 
+    /**
+     * Delete Action
+     * <br/> Responsible for :
+     *       <br/>In case Get verb  -> Display Delete Exists Book Form
+     *       <br/>In case Post verb -> delete Exists book from Database
+     *                                 After Submit Book Form
+     * @return ViewModel edit view
+     */
     public function deleteAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
 
+        if (!$id) {
+
+            return $this->redirect()->toRoute('book');
+        }
+
+        $request = $this->getRequest();
+        // Check If Request if Post Verb
+        if ($request->isPost()) {
+
+        }
+
+        return new ViewModel(array(
+            'id'   => $id,
+            //'book' => $book
+        ));
     }
 
 }
