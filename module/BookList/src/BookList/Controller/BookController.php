e84bdc6ef361bdd61fb9bf06923d6f0bd4a0eb5e
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2015-07-13 23:56:36 +0200

    Controller and View part: Create Book Form class, update Index view, update Add view, Write Index login into Index Action in Book \Controller And Write Add logic (only in Get verb) into Add Action in Book Controller

--- module/BookList/src/BookList/Controller/BookController.php
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -2,20 +2,45 @@
 
 namespace BookList\Controller;
 
+use BookList\Form\BookForm;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 class BookController extends AbstractActionController
 {
-
+    /**
+     * Index Action
+     * <br/> Responsible for Display List of Books which saved into DataBase
+     * @return ViewModel index view
+     */
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel(array(
+            'books' => []
+        ));
     }
 
+    /**
+     * Add Action
+     * <br/> Responsible for :
+     *       <br/>In case Get verb  -> Display Add new Book Form
+     *       <br/>In case Post verb -> Add new book details into Database after Submit Book Form
+     * @return ViewModel add view
+     */
     public function addAction()
     {
+        $form = new BookForm();
+            $form->get('submit')->setValue('Add');
 
+        $request = $this->getRequest();
+        // Check If request is Post verb
+        if ($request->isPost()) {
+
+        }
+
+        return new ViewModel(array(
+            'form' => $form
+        ));
     }
 
     public function editAction()
---
+++ module/BookList/src/BookList/Form/BookForm.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BookList\Form;
+
+use Zend\Form\Form;
+
+class BookForm extends Form
+{
+    /**
+     * Book Form Constructor
+     * @param String|null $name name for Book form
+     */
+    public function __construct($name=null)
+    {
+        // we want to ignore the name of Form Which passed as parameter
+        parent::__construct("book");
+
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Hidden'
+        ));
+
+        $this->add(array(
+            'name'    => 'title',
+            'type'    => 'Text',
+            'options' => array(
+                'label' => 'Title'
+            )
+        ));
+
+        $this->add(array(
+            'name'    => 'author',
+            'type'    => 'Text',
+            'options' => array(
+                'label' => 'author'
+            )
+        ));
+
+        $this->add(array(
+            'name'       => 'submit',
+            'type'       => 'Submit',
+            'attributes' => array(
+                'value' => 'Go',
+                'id'    => 'submitbutton'
+            )
+        ));
+    }
+
+}
