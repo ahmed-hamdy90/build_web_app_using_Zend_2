4fc140f33ac5d5efb199285bfeabd76b2d7b681b
ahmed-hamdy90 <thespy2030@yahoo.com>
2015-09-06 00:32:55 +0300

    Zend Official User Guide part(Routing and Controller Page): Create Album Controllerwithin Four Actions for making Album CRUD opertions and create routing of Album Modules

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -1,1 +1,54 @@
 <?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Album Controller
+ * @package Album\Controller
+ * @author Ahmed Hamdy <ahmedhamdy20@gmail.com>
+ */
+class AlbumController extends AbstractActionController
+{
+    /**
+     * Index Action 
+     * <br/> Used to Display Album List
+     * @return ViewModel index view
+     */
+    public function indexAction()
+    {        
+        return new ViewModel();
+    }
+    
+    /**
+     * Add Action
+     * <br/> Used to create a new Album
+     * @return ViewModel add View 
+     */
+    public function addAction()
+    {        
+        return new ViewModel();        
+    }        
+    
+    /**
+     * Edit Action
+     * <br/> Used to Edit Exists Album
+     * @return ViewModel edit view
+     */
+    public function editAction()
+    {
+        return new ViewModel();        
+    }        
+    
+    /**
+     * Delete Action
+     * <br/> Used to delete Exists Album
+     * @return ViewModel delete view
+     */
+    public function deleteAction()
+    {
+        return new ViewModel();        
+    }        
+}
